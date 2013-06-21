3c853560d144ae94bea7afed76198b130a820060
Josh Bremer <josh@quangoinc.com>
2013-06-21 12:33:16 -0700

    QA page starts with only showing open issues

--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,12 @@ module QuangoAssurance
     # ===============================
     # If you're not using Heroku Include This
     # ===============================
-    # config.before_configuration do
-    #  env_file = File.join(Rails.root, 'config', 'config.yml')
-    #  YAML.load(File.open(env_file)).each do |key, value|
-    #    ENV[key.to_s] = value
-    #  end if File.exists?(env_file)
-    # end
+    config.before_configuration do
+     env_file = File.join(Rails.root, 'config', 'config.yml')
+     YAML.load(File.open(env_file)).each do |key, value|
+       ENV[key.to_s] = value
+     end if File.exists?(env_file)
+    end
     
     # Pusher API
     Pusher.app_id = ENV["PUSHER_API"]
